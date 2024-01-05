eca9c8af3bb038423be8d5d36eea17e77a7245e7
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-01-05 12:27:57 +0530

    Update MongoDB URL and add CORS middleware

--- backend/config.js
+++ backend/config.js
@@ -3,5 +3,5 @@
 // Path: backend/config.js
 
 export const PORT = 5555
-export const MONGO_URL =
+export const mongoDBURL =
 	"mongodb+srv://megi:megi123@cluster0.plqgahu.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,41 @@
-import express from "express"
-import { PORT, MONGO_URL } from "./config.js";
-import mongoose from "mongoose"
-import { Book } from "./models/bookModel.js";
-import bookRouter from "./routes/booksRoute.js";
-const app = express()
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
-app.use(express.json())
+const app = express();
 
+// Middleware for parsing request body
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Welocme to Book API")
-})
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
-app.use("/books", bookRouter);
+app.get("/", (request, response) => {
+	console.log(request);
+	return response.status(234).send("Welcome To MERN Stack Tutorial");
+});
 
-mongoose.connect(MONGO_URL).then(()=>{
-    console.log("Successfully connected to MongoDB")
-    app.listen(PORT, () => {
-			console.log(`App is running on the server on port ${PORT}`);
+app.use("/books", booksRoute);
+
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log("App connected to database");
+		app.listen(PORT, () => {
+			console.log(`App is listening to port: ${PORT}`);
 		});
-}).catch((err)=>{
-    console.log("Error connecting to MongoDB",err)
-})
-
+	})
+	.catch((error) => {
+		console.log(error);
+	});
