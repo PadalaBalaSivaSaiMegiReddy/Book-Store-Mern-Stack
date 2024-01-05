725aea5d4ffc2d6299acdbb8cb59a87beb60687b
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-01-05 19:19:00 +0530

    Add routes for Home, CreateBooks, ShowBook, EditBook, and DeleteBook pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes,Route } from "react-router-dom"
+import Home from "./pages/Home";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+import CreateBooks from "./pages/CreateBooks";
+import ShowBook from "./pages/ShowBook";
 
 const App = () => {
   return (
 		<Routes>
-			<Route path="" />
-			<Route path="" />
-			<Route path="" />
-			<Route path="" />
-			<Route path="" />
-			<Route path="" />
+			<Route path="/" element={<Home/>}/>
+			<Route path="/books/create" element={<CreateBooks/>} />
+			<Route path="/books/details/:id" element={<ShowBook/>} />
+			<Route path="/books/edit/:id" element={<EditBook/>} />
+			<Route path="/books/delete/:id" element={<DeleteBook/>} />
 		</Routes>
 	);
 }
