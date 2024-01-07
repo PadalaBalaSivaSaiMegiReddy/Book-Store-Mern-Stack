aa64ec1dc2fe2ea49ce9e3a72d46a00ff717dfb3
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-01-07 17:38:57 +0530

    Update favicon and add colons to labels in ShowBook component

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -32,27 +32,27 @@ const ShowBook = () => {
 			) : (
 				<div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
 					<div className="my-4">
-						<span className="text-xl mr-4 text-gray-500">Id</span>
+						<span className="text-xl mr-4 text-gray-500">Id:</span>
 						<span>{book._id}</span>
 					</div>
 					<div className="my-4">
-						<span className="text-xl mr-4 text-gray-500">Title</span>
+						<span className="text-xl mr-4 text-gray-500">Title:</span>
 						<span>{book.title}</span>
 					</div>
 					<div className="my-4">
-						<span className="text-xl mr-4 text-gray-500">Author</span>
+						<span className="text-xl mr-4 text-gray-500">Author:</span>
 						<span>{book.author}</span>
 					</div>
 					<div className="my-4">
-						<span className="text-xl mr-4 text-gray-500">Publish Year</span>
+						<span className="text-xl mr-4 text-gray-500">Publish Year:</span>
 						<span>{book.publishYear}</span>
 					</div>
 					<div className="my-4">
-						<span className="text-xl mr-4 text-gray-500">Create Time</span>
+						<span className="text-xl mr-4 text-gray-500">Create Time:</span>
 						<span>{new Date(book.createdAt).toString()}</span>
 					</div>
 					<div className="my-4">
-						<span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+						<span className="text-xl mr-4 text-gray-500">Last Update Time:</span>
 						<span>{new Date(book.updatedAt).toString()}</span>
 					</div>
 				</div>
